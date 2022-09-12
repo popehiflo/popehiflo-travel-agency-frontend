072e759f11b5ff72fa6c702227614874a848e743
popehiflo <poolpetterhijuel@hotmail.com>
2022-09-12 15:45:09 -0500

    feat: Header component created

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,8 @@
-import logo from './logo.svg';
+import Header from './components/Header';
+// import logo from './logo.svg';
 
 const App = () => (
-  <section className="App">
-    <div className="app">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>
-        Edit.
-      </p>
-      <div className="heading">
-        <h1>h1</h1>
-        <span>Heading span</span>
-      </div>
-      <a href="https://reactjs.org" className="btn-primary">Boton primario 1</a>
-      <a href="https://reactjs.org" className="btn-primary">Boton primario 2</a>
-      <a href="https://reactjs.org" className="btn-primary">Boton primario 2</a>
-      <a href="https://reactjs.org" className="btn-primary">Boton primario 2</a>
-      <a href="https://reactjs.org" className="btn-primary">Boton primario 2</a>
-      <a href="https://reactjs.org" className="btn-primary">Boton primario 2</a>
-      <a href="https://reactjs.org" className="btn-icon-primary">A</a>
-      <a href="https://reactjs.org" className="btn-icon-primary">B</a>
-      <a href="https://reactjs.org" className="btn-icon-primary">C</a>
-    </div>
-  </section>
+  <Header />
 );
 
 export default App;
