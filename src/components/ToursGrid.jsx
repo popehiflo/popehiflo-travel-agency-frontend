a8f0b5f9589fa48b00ecfa7b474fb9f4e86cbed4
popehiflo <poolpetterhijuel@hotmail.com>
2022-09-13 17:17:08 -0500

    feat: ToursGrid component with mock data

--- src/components/ToursGrid.jsx
+++ src/components/ToursGrid.jsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import styledComponents from 'styled-components';
+import { getAllTours } from '../MOCKDATA';
 
-const ToursGrid = () => (
-  <div>ToursGrid</div>
-);
+const GridWrapper = styledComponents.div`
+
+`;
+
+const ToursGrid = () => {
+  const tours = getAllTours();
+  return (
+    <GridWrapper>
+      {tours.map((tour) => (
+        <pre>{JSON.stringify(tour, null, 2)}</pre>
+      ))}
+    </GridWrapper>
+  );
+};
 
 export default ToursGrid;
