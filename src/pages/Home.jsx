f578d9653b08672acd467c690d1066e239c13966
popehiflo <poolpetterhijuel@hotmail.com>
2022-09-15 11:15:11 -0500

    feat: About component, created

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import About from '../components/About';
 import Banner from '../components/Banner';
 import TourCategories from '../components/TourCategories';
 
 const Home = () => (
   <>
     <Banner />
+    <About />
     <TourCategories />
   </>
 );
