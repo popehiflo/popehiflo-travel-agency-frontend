2e8b88af07eac7d098b446feb143dc4114739231
popehiflo <poolpetterhijuel@hotmail.com>
2022-09-14 22:41:28 -0500

    feat: TourTypes component created

---
+++ src/components/TourTypes.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const TourTypesWrapper = styled.section``;
+
+const TourTypes = () => (
+  <TourTypesWrapper>
+    TourTypes
+  </TourTypesWrapper>
+);
+
+export default TourTypes;
